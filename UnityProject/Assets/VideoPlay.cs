0886b6b88484688ebb670128577992b7df5c942a
n163lph03n1x <myke_wills@hotmail.com>
2021-02-09 23:54:00 -0400

    First Upload

---
+++ UnityProject/Assets/VideoPlay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoPlay : MonoBehaviour {
+    public MovieTexture movTexture;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (movTexture.isPlaying)
+        {
+
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+}
